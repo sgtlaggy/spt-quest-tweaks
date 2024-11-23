2e6d25efca6e9f65b70da9ea05d191069f14790c
sgtlaggy <8661717+sgtlaggy@users.noreply.github.com>
2024-11-23 15:15:28 -0800

    remove gunsmith setting
    3.10 requires all ids to be MongoIDs and there isn't a very clean way
    to do this reliably without hardcoding 26+ ids

--- src/config.ts
+++ src/config.ts
@@ -1,44 +1,10 @@
 import config from "../config/config.json";
 
-class IGunsmithChallengeConfig {
-        killsRequired: number
-        targetType: string
-}
-
 class IConfig {
     revealAllQuestObjectives: boolean
     removeTimeGates: boolean
     addMissingSetupShotguns: boolean
     lightkeeperOnlyRequireLevel: number
-    gunsmithChallenge: IGunsmithChallengeConfig
-}
-
-
-// targetType: [[ScavType], Faction, SingularName, PluralSuffix]
-export const gunsmithChallengeTargetTypes = {
-    any: [[], "Any", "target", "s"],
-    pmc: [[], "AnyPmc", "PMC operative", "s"],
-    usec: [[], "Usec", "USEC PMC operative", "s"],
-    bear: [[], "Bear", "BEAR PMC operative", "s"],
-    scav: [[], "Savage", "scav", "s"],
-    raider: [["pmcBot"], "Savage", "raider", "s"],
-    rogue: [["exUsec"], "Savage", "rogue", "s"],
-    cultist: [["sectantPriest", "sectantWarrior"], "Savage", "cultist", "s"],
-    boss: [[
-        "bossBully",            // reshala
-        "bossKilla",
-        "bossGluhar",
-        "bossKojaniy",          // shturman
-        "bossSanitar",
-        "bossTagilla",
-        "bossZryachiy",
-        "bossKolontay",
-        "bossBoar",             // kaban
-        "bossKnight",
-        "followerBigPipe",
-        "followerBirdEye",
-        "sectantPriest"
-    ], "Savage", "boss", "es"]
 }
 
 export const CONFIG: IConfig = config;
--- src/mod.ts
+++ src/mod.ts
@@ -7,7 +7,7 @@ import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
 import { ILogger } from "@spt/models/spt/utils/ILogger";
 import { DatabaseService } from "@spt/services/DatabaseService";
 
-import { CONFIG, gunsmithChallengeTargetTypes } from "./config";
+import { CONFIG } from "./config";
 
 
 export const IDS = {
@@ -36,11 +36,7 @@ class Mod implements IPostDBLoadMod {
         const log = (msg: string) => logger.info(`[QuestTweaks] ${msg}`);
 
         const db = container.resolve<DatabaseService>("DatabaseService");
-        const locales = db.getLocales().global;
         const quests = db.getQuests();
-
-        const [scavType, target, targetSingular, targetPluralSuffix] = gunsmithChallengeTargetTypes[CONFIG.gunsmithChallenge.targetType.toLowerCase()] || [undefined, undefined, undefined, undefined];
-        const targetName = `${targetSingular}${CONFIG.gunsmithChallenge.killsRequired > 1 ? targetPluralSuffix : ""}`;
 
         if (CONFIG.revealAllQuestObjectives) {
             log("Revealing hidden/conditional objectives.");
@@ -69,15 +65,6 @@ class Mod implements IPostDBLoadMod {
             ];
         }
 
-        if (CONFIG.gunsmithChallenge.killsRequired > 0) {
-            if (scavType === undefined) {
-                log(`${CONFIG.gunsmithChallenge.targetType} is not a valid target type, skipping.\nValid options: ${Object.keys(gunsmithChallengeTargetTypes).join(", ")}`);
-                CONFIG.gunsmithChallenge.killsRequired = 0;
-            } else {
-                log(`Eliminate ${CONFIG.gunsmithChallenge.killsRequired} ${targetName} with each Gunsmith weapon.`);
-            }
-        }
-
         for (const quest of Object.values(quests)) {
             if (CONFIG.revealAllQuestObjectives) {
                 for (const objective of quest.conditions.AvailableForFinish) {
@@ -91,66 +78,6 @@ class Mod implements IPostDBLoadMod {
                         prereq.availableAfter = 0;
                     }
                 }
-            }
-
-            if (CONFIG.gunsmithChallenge.killsRequired > 0 && quest.QuestName?.startsWith("Gunsmith")) {
-                quest.conditions.AvailableForFinish.filter((cond) => {
-                    return cond.conditionType === QuestTypeEnum.WEAPON_ASSEMBLY;
-                }).map((weaponCondition) => {
-                    const localeKey = `${quest._id} ${weaponCondition.id} challengeElims`;
-
-                    // @ts-ignore                  containsItems undeclared/undocumented
-                    const requiredMods: string[] = weaponCondition.containsItems;
-
-                    const elimCondition: IQuestCondition = {
-                        id: localeKey,
-                        value: CONFIG.gunsmithChallenge.killsRequired,
-                        conditionType: "CounterCreator",
-                        counter: {
-                            id: `${localeKey} counter`,
-                            conditions: [
-                                {
-                                    bodyPart: [],
-                                    compareMethod: ">=",
-                                    conditionType: "Kills",
-                                    daytime: {
-                                        from: 0,
-                                        to: 0
-                                    },
-                                    distance: {
-                                        compareMethod: ">=",
-                                        value: 0
-                                    },
-                                    dynamicLocale: false,
-                                    enemyEquipmentExclusive: [],
-                                    enemyEquipmentInclusive: [],
-                                    enemyHealthEffects: [],
-                                    id: `${localeKey} condition`,
-                                    resetOnSessionEnd: false,
-                                    savageRole: scavType,
-                                    target: target,
-                                    value: 1,
-                                    weapon: [weaponCondition.target].flat(),
-                                    weaponCaliber: [],
-                                    weaponModsExclusive: [],
-                                    weaponModsInclusive: requiredMods ? [requiredMods] : []
-                                }
-                            ]
-                        },
-                        dynamicLocale: false,
-                        completeInSeconds: 0,
-                        target: []
-                    };
-                    return elimCondition;
-                }).forEach((cond) => {
-                    quest.conditions.AvailableForFinish.push(cond);
-
-                    const weaponId = cond.counter.conditions[0].weapon[0];
-                    const weapon = locales.en[`${weaponId} ShortName`];
-                    for (const locale of Object.values(locales)) {
-                        locale[cond.id] = `Eliminate ${CONFIG.gunsmithChallenge.killsRequired} ${targetName} with the ${weapon}`;
-                    }
-                });
             }
         }
     }
